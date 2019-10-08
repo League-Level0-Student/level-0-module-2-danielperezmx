32aacc04e692d356c59b601a348623d4191a81a9
ilabstaff <ilabstaff@172.16.116.79>
2019-10-07 19:27:46 -0700

    magic 8ball

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -17,6 +17,37 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
+if (randomNumber == 1) {
+	JOptionPane.showMessageDialog(null, "howdy there partner");
+	
+}
+else if(randomNumber==1);
+
+if (randomNumber == 2) {
+	JOptionPane.showMessageDialog(null, "ahoy there matie");
+	
+}
+else if(randomNumber==2);
+
+if (randomNumber == 3) {
+	JOptionPane.showMessageDialog(null, "you are nice");
+	
+}
+else if(randomNumber==3);
+
+if (randomNumber == 4) {
+	JOptionPane.showMessageDialog(null, "you have some nice shoes");
+	
+}
+else if(randomNumber==4);
+
+if (randomNumber == 5) {
+	JOptionPane.showMessageDialog(null, "man, I wish I was as cool as you");
+	
+}
+else if(randomNumber==5);
+
+
 
 		// 2. Repeat all the code above 10 times
 		
--- src/_05_magic_8_ball/Magic8Ball.java
+++ src/_05_magic_8_ball/Magic8Ball.java
@@ -6,13 +6,27 @@ package _05_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+public static void main(String[] args) {
+	Random randomMaker = new Random();
+	int name=  randomMaker.nextInt(4);
+		
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
 	// 3. Print out this variable
+JOptionPane.showInputDialog("what would you like to ask the all mighty 8 ball?");
+
+if (name == 0) {
+	JOptionPane.showMessageDialog(null, "yes");
+}
+else if (name == 1)
+{}
+	
 
 	// 4. Get the user to enter a question for the 8 ball
 
@@ -31,5 +45,5 @@ public class Magic8Ball {
 	// 8. If the random number is 3
 
 	// -- write your own answer
-
+}
 }
